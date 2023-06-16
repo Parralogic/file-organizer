f9ca1c7a88906ab8ea030c9eefb56486e290d81a
Parralogic <Parralogic@gmail.com>
2023-06-16 04:42:49 -0700

    06/16/2023

--- READme.txt
+++ READme.txt
@@ -1,9 +1,17 @@
 This is a simple script/s that will organize your files
 in a calendar and date layout; good for the office to organize your shit. still working on it.... as of 03/29/2023.
 
+UPDATE! 06/16/2023
+Finished the file-organizer.sh script, its simple enough that anyone with little knowledge in scripting can modify
+to their liking. So for this script/s to work; first run/execute the layout.sh script,
+then place the file-organizer.sh and INFO file in the SINGLE directory where all your files are stored [don't store files in a directory within the directory that your organizing],
+so they can be organized. YES place all the files in a SINGLE directory/folder; they will be organized to the Month and Day
+based on the layout INFO file.
+
+
 UPDATE! 04/04/2023
 Made a little change in the layout.sh script. It will create an INFO file of the directory
-where you executed/run the layout.sh script. Now, need to work on the file-organize.sh script.
+where you executed/run the layout.sh script. Now, need to work on the file-organizer.sh script.
 And yes Im taking my time; Not like the bouncer2.4_5GHz|THEone.
 
 
@@ -13,5 +21,3 @@ and within the Months will create the subfolders for the Days. That way you can 
 on the Month and Day; My advice would be to create a folder of the YEAR then run/execute the layout.sh script
 in that directory/folder. Then just right click and sort by Created, so the folders of the Months would
 be in order.
-
-testing acode
--- file-organizer.sh
+++ file-organizer.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 #Creator: David Parra-Sandoval
-#Date:
-#Last Modified:
+#Date: 06/16/2023
+#Last Modified: 06/16/2023
 clear
 
+MONTH=$(date +%B)                  ### DON'T touch/Modify
+DAY=$(date +%d)                    ### DON'T touch/Modify
+BACKUPLOCATION=$(cat INFO)         ### DON'T touch/Modify
 
+mkdir $BACKUPLOCATION/$MONTH/$DAY/TXT &> /dev/null  ### This creates a directory TXT
+mkdir $BACKUPLOCATION/$MONTH/$DAY/DOC &> /dev/null  ### you can modify this to your
+mkdir $BACKUPLOCATION/$MONTH/$DAY/PDF &> /dev/null  ### liking. You can add more
+mkdir $BACKUPLOCATION/$MONTH/$DAY/ZIP &> /dev/null  ### or delete!
+mkdir $BACKUPLOCATION/$MONTH/$DAY/JPEG &> /dev/null ### If you add more directories you need to change the bottom too! Follow the pattern.
+                                                    ### "*" is a wildcard; in this script it means whatever is in this directory do this..
+                                                    ### for loop takes every file one by one and puts it in a variable named FILES and a case statement;multiple ##  different choices of files, do this.
+
+
+for FILES in *; do
+case $FILES in
+*.txt | *.TXT | *.Txt | *.txT | *.tXt)
+mv $FILES $BACKUPLOCATION/$MONTH/$DAY/TXT
+;;
+*.doc | *.DOC | *.Doc | *.doC | *.dOc)
+mv $FILES $BACKUPLOCATION/$MONTH/$DAY/DOC
+;;
+*.pdf | *.PDF | *.Pdf | *.pdF | *.pDf)
+mv $FILES $BACKUPLOCATION/$MONTH/$DAY/PDF
+;;
+*.zip | *.ZIP | *.Zip | *.ziP | *.zIp)
+mv $FILES $BACKUPLOCATION/$MONTH/$DAY/ZIP
+;;
+*.jpeg | *.JPEG | *.Jpeg | *.jpeG | *.jpg | *.gif | *.png | *.PNG | *.GIF)
+mv $FILES $BACKUPLOCATION/$MONTH/$DAY/JPEG
+;;
+esac
+done
